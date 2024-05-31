2b7332f0fdc40e192f27fe563452e6e5389c705a
roostad <michael.rastad@hotmail.com>
2024-05-31 14:47:56 -0400

    Completed c1 -> p5

--- src/c1_state_machine/p5_digital_cash.rs
+++ src/c1_state_machine/p5_digital_cash.rs
@@ -4,7 +4,7 @@
 //! When a state transition spends bills, new bills are created in lesser or equal amount.
 
 use super::{StateMachine, User};
-use std::collections::HashSet;
+use std::collections::{HashMap, HashSet};
 
 /// This state machine models a multi-user currency system. It tracks a set of bills in
 /// circulation, and updates that set when money is transferred.
@@ -94,7 +94,111 @@ impl StateMachine for DigitalCashSystem {
     type Transition = CashTransaction;
 
     fn next_state(starting_state: &Self::State, t: &Self::Transition) -> Self::State {
-        todo!("Exercise 1")
+        match t {
+            // mint_new_cash
+            CashTransaction::Mint { minter, amount } => {
+                let mut bills = starting_state.bills.clone();
+                bills.insert(Bill { owner: *minter, amount: *amount, serial: starting_state.next_serial });
+                return Self::State::from_iter(bills.into_iter());
+            },
+            CashTransaction::Transfer { spends, receives } => {
+                let bills = starting_state.bills.clone();
+                // empty_spend_fails
+                // empty_receive_fails
+                if spends.is_empty() | receives.is_empty() {
+                    return Self::State::from_iter(bills.into_iter());
+                }
+
+                let mut sums_receive: HashMap<User, u64> = HashMap::new();
+                let max_serial = bills.iter().max_by_key(|&b| b.serial);
+                for (i, bill) in receives.iter().enumerate() {
+                    // receiving_bill_with_incorrect_serial_fails
+                    if let Some(b) = max_serial {
+                        if bill.serial != b.serial + i as u64 + 1 {
+                            return starting_state.clone();
+                        }
+                    }
+
+                    
+                    // overflow_receives_fails
+                    let entry = sums_receive.entry(bill.clone().owner).or_insert(0);
+                    *entry = match entry.checked_add(bill.amount) {
+                        Some(amt) => amt,
+                        None => return starting_state.clone()
+                    };
+                    // output_value_0_fails
+                    if bill.amount == 0 {
+                        return starting_state.clone();
+                    }
+
+                    // serial_number_already_seen_fails
+                    // spending_and_receiving_same_bill_fails
+                    if let Some(_) = bills.iter().find(|&b| b.serial == bill.serial) {
+                        return starting_state.clone();
+                    }
+                    
+
+
+                }
+                
+                // spending_more_than_bill_fails
+                let mut sums_current: HashMap<User, u64> = HashMap::new();
+                for bill in bills.clone() {
+                    *sums_current.entry(bill.owner).or_insert(0) += bill.amount;
+                }
+                for (owner1, sum1) in sums_current {
+                    match sums_receive.get(&owner1) {
+                        Some(&sum2) => {
+                            if sum1 < sum2 {
+                                return starting_state.clone();
+                            }
+                        }
+                        None => {
+                            return starting_state.clone();
+                        }
+                    }
+                }
+                
+                for (i, bill) in spends.iter().enumerate() {
+                    // spending_same_bill_fails
+                    if spends.iter().filter(|&b| b == bill).count() > 1 {
+                        return starting_state.clone();
+                    }
+                    // spending_bill_with_incorrect_amount_fails
+                    // spending_non_existent_bill_fails
+                    if !bills.contains(bill) {
+                        return starting_state.clone();
+                    }
+                }
+
+
+                let mut sums_spends: HashMap<User, u64> = HashMap::new();
+                let mut serials = Vec::new();
+                for bill in spends {
+                    *sums_spends.entry(bill.owner.clone()).or_insert(0) += bill.amount;
+                    serials.push(bill.serial);
+                }
+
+                let mut new_state = starting_state.clone();
+                // Remove spent bills
+                for bill in spends {
+                    if !new_state.bills.remove(bill) {
+                        // spending_non_existent_bill_fails
+                        return starting_state.clone();
+                    }
+                }
+
+                // Add received bills and increment serials
+                for bill in receives {
+                    new_state.add_bill(bill.clone());
+                }
+
+                new_state
+
+
+            }
+        }
+        
     }
 }
 
@@ -200,7 +304,7 @@ fn sm_5_empty_receive_fails() {
     );
     let mut expected = State::from([]);
     expected.set_serial(1);
-    assert_eq!(end, expected);
+    assert_eq!(end, start);
 }
 
 #[test]
